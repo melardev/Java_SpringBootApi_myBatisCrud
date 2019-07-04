c11bde50b915a623e539885b6dbd061d4367fab9
MelarDev <melardev@users.noreply.github.com>
2019-07-04 11:51:41 +0200

    Update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS `TODOS`
 (
-    `id`         INTEGER  PRIMARY KEY AUTO_INCREMENT,
-    completed   boolean not null,
-    created_at  timestamp,
-    description clob,
-    title       varchar(255),
-    updated_at  timestamp
+    `id`          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `title`       varchar(255) NOT NULL,
+    `description` clob,
+    `completed`   boolean      not null,
+    `created_at`  timestamp,
+    `updated_at`  timestamp
 )
+/*
+select * from TODOS;
+*/
